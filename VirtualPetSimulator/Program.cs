f36c00f527fbcfe5401abb7c97696e7eb137554f
Aayush Thapa <Athapa4126@conestogac.on.ca>
2025-01-16 23:42:55 -0500

    Added a gitignore file, started working on the pet action feature

--- VirtualPetSimulator/Program.cs
+++ VirtualPetSimulator/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 class Program
-{
+{   
     public static void Main(string[] args)
     {
         int pet_type;
@@ -44,10 +44,56 @@ class Program
             else
             {
                 Console.WriteLine("----------------------------------------");
-                Console.WriteLine("***Invalid Input. Please enter valid integer***");
+                Console.WriteLine("***Invalid Input. Please enter valid number***");
                 Console.WriteLine("----------------------------------------");
             }
         }
+        string petName = Console.ReadLine();
+        Console.WriteLine("----------------------------------------");
+        Console.Write("Pet Name: ");
+        Console.WriteLine($"Nice name, {petName}! Let's take a look on what we can do with {petName}.");
+        Console.WriteLine("----------------------------------------");
+
+        int petAction;
+        while(true)
+        {
+            Console.WriteLine($"Main Menu: \n1. Feed {petName}\n2. Play with {petName}\n3. Let {petName} Rest");
+            Console.WriteLine($"4. Check {petName}'s Status\n5. Exit\n");
+            Console.Write("Enter your choice: ");
+            int hunger_level = 0;
+            int happiness_level = 0;
+            int health_level = 0;
+
+            if (int.TryParse(Console.ReadLine(), out petAction))
+            {
+                switch(petAction)
+                {
+                    case 1:
+                        Console.WriteLine($"Thanks for feeding {petName}. His hunger is gonna decrease and health increases.");
+                        hunger_level -= 1;
+                        Console.WriteLine(hunger_level);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("***Invalid Input. Please enter valid number***");
+                Console.WriteLine("----------------------------------------");
+            }
+
+        }
+
 
 
 
