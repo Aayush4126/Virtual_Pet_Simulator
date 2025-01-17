58f1a62aa12bcf06e8e8a025c3aa7ceb4d0f63b8
Aayush Thapa <Athapa4126@conestogac.on.ca>
2025-01-17 00:13:10 -0500

    Pet care actions and pet status monitoring feature completed, and also added the comments on the code

--- VirtualPetSimulator/Program.cs
+++ VirtualPetSimulator/Program.cs
@@ -4,21 +4,27 @@ class Program
 {   
     public static void Main(string[] args)
     {
+        // Declaring the variables and initializing the pet types
         int pet_type;
         string pet1 = "Dog";
         string pet2 = "Cat";
         string pet3 = "Horse";
+
+        //Displaying the welcome message 
         Console.WriteLine("\n\n\n\n-----------------Welcome to Virtual Pet Simulator------------");
         Console.WriteLine("\n\n----------------------------------------");
         Console.WriteLine($"Please choose a type of pet: \n1. {pet1}\n2. {pet2}\n3. {pet3}");
         Console.WriteLine("----------------------------------------");
 
+        //This loop will run until the user provides valid input
         while (true)
         {
             Console.Write("Enter the number of your pet: ");
 
+            //Getting the user input and storing it in pet_type. 
             if (int.TryParse(Console.ReadLine(), out pet_type))
             {
+                //Checking the pet selected by user and asking for the name
                 if (pet_type == 1)
                 {
                     Console.WriteLine($"You've chosen a {pet1}. What would you like to name your pet?");
@@ -48,6 +54,8 @@ class Program
                 Console.WriteLine("----------------------------------------");
             }
         }
+
+        //Getting the pet name from the user
         string petName = Console.ReadLine();
         Console.WriteLine("----------------------------------------");
         Console.Write("Pet Name: ");
@@ -55,32 +63,56 @@ class Program
         Console.WriteLine("----------------------------------------");
 
         int petAction;
-        while(true)
+
+        // Initializing the level of the pet
+        int hunger_level = 0;
+        int happiness_level = 0;
+        int health_level = 0;
+
+        //
+        while (true)
         {
             Console.WriteLine($"Main Menu: \n1. Feed {petName}\n2. Play with {petName}\n3. Let {petName} Rest");
             Console.WriteLine($"4. Check {petName}'s Status\n5. Exit\n");
             Console.Write("Enter your choice: ");
-            int hunger_level = 0;
-            int happiness_level = 0;
-            int health_level = 0;
+            
 
             if (int.TryParse(Console.ReadLine(), out petAction))
             {
                 switch(petAction)
                 {
                     case 1:
+                        //Action for feeding the pet
                         Console.WriteLine($"Thanks for feeding {petName}. His hunger is gonna decrease and health increases.");
                         hunger_level -= 1;
-                        Console.WriteLine(hunger_level);
+                        health_level += 1;
                         break;
                     case 2:
+                        //Action for playing with pet
+                        Console.WriteLine($"Thanks for playing with {petName}. His hunger level and happiness is going to increase.");
+                        hunger_level += 1;
+                        happiness_level += 1;
                         break;
                     case 3:
+                        //Action for resting the pet
+                        Console.WriteLine($"Thanks for letting {petName} rest. His health is now going to improve but happiness may decreses.");
+                        health_level += 1;
+                        happiness_level -= 1;
                         break;
                     case 4:
+                        //Checking the status of the pet
+                        Console.WriteLine("----------------");
+                        Console.WriteLine($"{petName}'s Status: ");
+                        Console.WriteLine("----------------");
+                        Console.WriteLine($"|Hunger:    {hunger_level}");
+                        Console.WriteLine($"|Happiness: {happiness_level}");
+                        Console.WriteLine($"|Health:    {health_level}");
+                        Console.WriteLine("----------------");
                         break;
                     case 5:
-                        break;
+                        //Exiting the game
+                        Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+                        return; //Exitting the program
                     default:
                         break;
                 }
